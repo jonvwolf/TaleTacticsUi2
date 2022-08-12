12817e50becd73c87087c8c4551ef91fa642509b
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2022-08-11 20:17:19 -0500

    WIP

--- src/app/core/api-models/read-game-state-model.ts
+++ src/app/core/api-models/read-game-state-model.ts
@@ -1,13 +1,11 @@
 import { defaultReadStoryModel, ReadStoryModel } from "./read-story-model";
 
 export const defaultReadGameStateModel:ReadGameStateModel = {
-    id: 0,
     code: '',
     story: defaultReadStoryModel
 };
 
 export interface ReadGameStateModel {
-    id:number,
     code:string,
     story:ReadStoryModel
 }
--- src/app/secured-app/game-list/game-list.component.ts
+++ src/app/secured-app/game-list/game-list.component.ts
@@ -1,10 +1,17 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { GamesEndpointsService } from 'src/app/core/api-endpoints/games-endpoints.service';
 import { ReadGameStateModel } from 'src/app/core/api-models/read-game-state-model';
 import { BaseFormComponent } from 'src/app/ui-helpers/base-form-component';
+
+interface IGameItem {
+  game:ReadGameStateModel,
+  code:string,
+  story:string
+}
 
 @Component({
   selector: 'app-game-list',
@@ -13,13 +20,13 @@ import { BaseFormComponent } from 'src/app/ui-helpers/base-form-component';
 })
 export class GameListComponent extends BaseFormComponent implements OnInit, AfterViewInit {
 
-  public tableColumns:string[] = ['title', 'story', 'buttons'];
-  public dataSource:MatTableDataSource<ReadGameStateModel> = new MatTableDataSource();
+  public tableColumns:string[] = ['code', 'story', 'buttons'];
+  public dataSource:MatTableDataSource<IGameItem> = new MatTableDataSource();
 
   @ViewChild(MatPaginator) paginator: MatPaginator|null = null;
   @ViewChild(MatSort) sort: MatSort|null = null;
 
-  constructor(private endpoints:GamesEndpointsService) {
+  constructor(private endpoints:GamesEndpointsService, private snackBar:MatSnackBar) {
     super();
   }
 
@@ -44,11 +51,7 @@ export class GameListComponent extends BaseFormComponent implements OnInit, Afte
     this.startLoadAndClearErrors();
     this.subs.add(this.endpoints.getAll().subscribe({
       next: (data) => {
-        let id = 1;
-        for(let item of data){
-          item.id = id++;
-        }
-        this.dataSource.data = data;
+        this.dataSource.data = data.map((x) => <IGameItem>{ code: x.code, game: x, story: x.story.title });
         this.endLoad();
       },
       error: (err) => {
@@ -57,11 +60,31 @@ export class GameListComponent extends BaseFormComponent implements OnInit, Afte
     }));
   }
 
-  public deleteGame(game:ReadGameStateModel):void{
+  public deleteGame(game:IGameItem):void{
+    if(confirm('Are you sure you delete this game? Code: ' + game.code)){
+      this.startLoadAndClearErrors();
+      this.subs.add(this.endpoints.delete(game.code).subscribe({
+        next: (data) => {
+          const list = this.dataSource.data;
+          const index = list.findIndex((item) => item.code === game.code);
+          list.splice(index, 1);
+          this.dataSource.data = list;
 
+          this.endLoad();
+          // TODO: this should be a common function or just for the config rather
+          this.snackBar.open('Game deleted', 'Close', {
+            duration: 8000,
+            panelClass: ['success-snack-bar']
+          });
+        },
+        error: (err) => {
+          this.endLoadAndHandleError(err);
+        }
+      }));
+    }
   }
 
-  public resume(game:ReadGameStateModel):void{
+  public resume(game:IGameItem):void{
 
   }
 }
